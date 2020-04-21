83e6b7ee7f95f452acde55597e383104f9428ba4
Ronmarsiano <ronmarsiano@gmail.com>
2020-04-21 21:55:57 +0300

    dev

--- lib/logstash/logAnalyticsClient/syslogClient.rb
+++ lib/logstash/logAnalyticsClient/syslogClient.rb
@@ -65,8 +65,8 @@ class SyslogClient
     host = "MyMachine"
     @logger.error("!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     @logger.error(timestamp.to_s)
-    @logger.error(host.to_s
-    @logger.error(document.get('MSG').to_s)
+    @logger.error(host.to_s)
+    @logger.error(document.get("MSG").to_s)
     @logger.error("!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     
     syslog_message = "<34>#{timestamp} #{host} #{document.get("MSG").to_s}"
